3b66bd4718954da70c84acdd22b91c633243cdbe
arunbhardwaj <arun.b.95@gmail.com>
2023-04-14 15:17:18 -0400

    uses LeetCode's API to make a request for submission details instead of scraping the page

--- scripts/leetcode.js
+++ scripts/leetcode.js
@@ -255,6 +255,7 @@ function uploadGit(
     .catch(console.error);
 }
 
+/* Gets updated GitHub data for the specific file in repo in question */
 async function getUpdatedData(token, hook, directory, filename) {
   const URL = `https://api.github.com/repos/${hook}/contents/${directory}/${filename}`;
 
@@ -298,24 +299,6 @@ function convertToSlug(string) {
     .replace(/-+$/, ''); // Trim - from end of text
 }
 
-function getProblemNameSlug() {
-  const questionElem = document.getElementsByClassName('content__u3I1 question-content__JfgR');
-  const questionDescriptionElem = document.getElementsByClassName('question-description__3U1T');
-  let questionTitle = 'unknown-problem';
-  if (checkElem(questionElem)) {
-    let qtitle = document.getElementsByClassName('css-v3d350');
-    if (checkElem(qtitle)) {
-      questionTitle = qtitle[0].innerHTML;
-    }
-  } else if (checkElem(questionDescriptionElem)) {
-    let qtitle = document.getElementsByClassName('question-title');
-    if (checkElem(qtitle)) {
-      questionTitle = qtitle[0].innerText;
-    }
-  }
-  return addLeadingZeros(convertToSlug(questionTitle));
-}
-
 function addLeadingZeros(title) {
   const maxTitlePrefixLength = 4;
   var len = title.split('-')[0].length;
@@ -353,30 +336,6 @@ document.addEventListener('click', event => {
         uploadGit(addition, problemName, 'README.md', discussionMsg, 'update', true);
       }
     }, 1000);
-  }
-});
-
-/* Sync to local storage */
-chrome.storage.local.get('isSync', data => {
-  keys = [
-    'leethub_token',
-    'leethub_username',
-    'pipe_leethub',
-    'stats',
-    'leethub_hook',
-    'mode_type',
-  ];
-  if (!data || !data.isSync) {
-    keys.forEach(key => {
-      chrome.storage.sync.get(key, data => {
-        chrome.storage.local.set({ [key]: data[key] });
-      });
-    });
-    chrome.storage.local.set({ isSync: true }, data => {
-      console.log('LeetHub Synced to local values');
-    });
-  } else {
-    console.log('LeetHub Local storage already synced!');
   }
 });
 
@@ -740,18 +699,51 @@ LeetCodeV2.prototype.parseStats = function () {
 
   return `Time: ${time} (${timePercentile}), Space: ${space} (${spacePercentile}) - LeetHub`;
 };
-LeetCodeV2.prototype.parseQuestion = function () {
+LeetCodeV2.prototype.parseQuestion = async function () {
   // Description
   let qBody = this.parseQuestionDescription();
+  console.log("🚀 ~ file: leetcode.js:705 ~ qBody:", qBody)
 
   // Problem title.
-  let qTitle = this.parseQuestionTitle();
-  let qNum = this.parseQuestionNumber();
-  qNum = parseInt(qNum, 10); // remove leading zeros
-  qTitle = `${qNum}. ${qTitle}`;
+  const titleSlug = convertToSlug(this.parseQuestionTitle())
+  console.log("🚀 ~ file: leetcode.js:708 ~ qTitle:", qTitle)
+  console.log("cookie", document.cookie)
 
+  // Parse SubmissionId
+  let URL = window.location.href
+  let submissionId = parseInt(URL.split('/submissions/')[1].slice(0, -1))
+  
+  // This query will give you the solution runtime and memory stats, the code, the coding language, the question id, question title and question difficulty
+  // let questionQuery = {"query":"\n    query questionTitle($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    title\n    titleSlug\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n  }\n}\n    ","variables":{"titleSlug":titleSlug},"operationName":"questionTitle"}
+  let submissionDetailsQuery = {"query":"\n    query submissionDetails($submissionId: Int!) {\n  submissionDetails(submissionId: $submissionId) {\n    runtime\n    runtimeDisplay\n    runtimePercentile\n    runtimeDistribution\n    memory\n    memoryDisplay\n    memoryPercentile\n    memoryDistribution\n    code\n    timestamp\n    statusCode\n    lang {\n      name\n      verboseName\n    }\n    question {\n      questionId\n    title\n    titleSlug\n    difficulty\n    }\n    notes\n    topicTags {\n      tagId\n      slug\n      name\n    }\n    runtimeError\n  }\n}\n    ","variables":{"submissionId":submissionId},"operationName":"submissionDetails"}
+  let options = {
+    method: 'POST',
+    headers: {
+      cookie: document.cookie,
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(submissionDetailsQuery)
+  }
+  const data = await fetch('https://leetcode.com/graphql/', options)
+    .then(res => res.json())
+    .then(res => res.data)
+  console.log("🚀 ~ file: leetcode.js:723 ~ data:", data)
+  
+
+  // sample response data
+  // {"data":{"submissionDetails":{"runtime":67,"runtimeDisplay":"67 ms","runtimePercentile":76.13729999999997,"runtimeDistribution":"{\"lang\": \"javascript\", \"distribution\": [[\"22\", 0.0058], [\"25\", 0.0019], [\"29\", 0.0039], [\"32\", 0.0019], [\"35\", 0.0019], [\"39\", 0.033100000000000004], [\"42\", 0.1422], [\"45\", 0.3646], [\"49\", 0.8482000000000001], [\"52\", 2.6790000000000003], [\"55\", 3.5174000000000003], [\"59\", 5.4144], [\"62\", 8.557500000000001], [\"65\", 6.8202], [\"69\", 5.9311], [\"72\", 4.2602], [\"75\", 3.1799999999999997], [\"78\", 1.2166000000000001], [\"82\", 0.7819], [\"85\", 0.8422000000000001], [\"88\", 0.8482000000000001], [\"92\", 1.2322], [\"95\", 2.5561], [\"98\", 2.6614], [\"102\", 3.295], [\"105\", 5.0888], [\"108\", 3.4374000000000002], [\"112\", 2.8642000000000003], [\"115\", 2.7277000000000005], [\"118\", 1.5539], [\"122\", 1.1835], [\"125\", 0.9476], [\"128\", 1.0471], [\"131\", 0.6317], [\"135\", 0.5908], [\"138\", 0.8286], [\"141\", 0.5362], [\"145\", 0.542], [\"148\", 0.7623], [\"151\", 0.5186000000000001], [\"155\", 0.5206000000000001], [\"158\", 0.7896000000000001], [\"161\", 0.8268], [\"165\", 1.0354], [\"168\", 1.6455], [\"171\", 1.3219], [\"175\", 1.5033], [\"178\", 1.396], [\"181\", 1.4292], [\"184\", 0.8248], [\"188\", 0.5517], [\"191\", 0.6004], [\"194\", 0.33530000000000004], [\"198\", 0.3159], [\"201\", 0.2807], [\"204\", 0.1559], [\"208\", 0.1774], [\"211\", 0.1988], [\"214\", 0.1521], [\"218\", 0.1384], [\"221\", 0.21430000000000002], [\"224\", 0.1169], [\"228\", 0.11699999999999999], [\"231\", 0.1091], [\"234\", 0.14029999999999998], [\"237\", 0.12279999999999999], [\"241\", 0.0877], [\"244\", 0.13649999999999998], [\"247\", 0.1072], [\"251\", 0.0857], [\"254\", 0.1482], [\"257\", 0.1364], [\"261\", 0.1111], [\"264\", 0.1617], [\"267\", 0.0877], [\"271\", 0.0857], [\"274\", 0.0934], [\"277\", 0.0721], [\"281\", 0.0857], [\"284\", 0.11299999999999999]]}","memory":45412000,"memoryDisplay":"45.4 MB","memoryPercentile":5.059500000000011,"memoryDistribution":"{\"lang\": \"javascript\", \"distribution\": [[\"39100\", 0.0019], [\"40500\", 0.0039], [\"40800\", 0.0078], [\"40900\", 0.0058], [\"41000\", 0.0214], [\"41100\", 0.0507], [\"41200\", 0.078], [\"41300\", 0.2281], [\"41400\", 0.2671], [\"41500\", 0.5713], [\"41600\", 1.1874], [\"41700\", 1.6475], [\"41800\", 2.3533], [\"41900\", 3.0708], [\"42000\", 3.9814], [\"42100\", 5.4515], [\"42200\", 6.4107], [\"42300\", 6.0988], [\"42400\", 6.4887], [\"42500\", 6.6837], [\"42600\", 6.3756], [\"42700\", 6.1046], [\"42800\", 4.4006], [\"42900\", 3.5271], [\"43000\", 3.4842], [\"43100\", 3.2132], [\"43200\", 3.1995], [\"43300\", 2.638], [\"43400\", 2.1038], [\"43500\", 1.8737], [\"43600\", 1.5247], [\"43700\", 1.2069], [\"43800\", 0.8618], [\"43900\", 0.6512], [\"44000\", 0.6122], [\"44100\", 0.5596], [\"44200\", 0.7019], [\"44300\", 0.7234], [\"44400\", 0.7292], [\"44500\", 0.774], [\"44600\", 0.7565], [\"44700\", 0.7838], [\"44800\", 0.7604], [\"44900\", 0.6493], [\"45000\", 0.5654], [\"45100\", 0.5128], [\"45200\", 0.3802], [\"45300\", 0.3393], [\"45400\", 0.3178]]}","code":"/**\n * v10 with event listener\n * @param {number[]} nums\n * @param {number} target\n * @return {number[]}\n */\nvar twoSum = function(nums, target) {\n    let subTarget = target\n    let mapArr = new Map();\n    \n    for (let i = 0; i < nums.length; i++) {        \n        subTarget = target - nums[i]\n        if (mapArr.has(subTarget)) {\n            return [i, mapArr.get(subTarget)]\n        }\n        mapArr.set(nums[i], [i])\n    }\n\n};","timestamp":1681498417,"statusCode":10,"user":{"username":"arunbhardwaj","profile":{"realName":"","userAvatar":"https://s3-us-west-1.amazonaws.com/s3-lc-upload/assets/default_avatar.jpg"}},"lang":{"name":"javascript","verboseName":"JavaScript"},"question":{"questionId":"1"},"notes":"","topicTags":[],"runtimeError":null,"compileError":null,"lastTestcase":""}}}
+  const qNum = data.submissionDetails.question.questionId
+  
+  // let qNum = this.parseQuestionNumber();
+  // console.log("🚀 ~ file: leetcode.js:710 ~ qNum:", qNum)
+  // qNum = parseInt(qNum, 10); // remove leading zeros
+  const qTitle = `${qNum}. ${qTitle}`;
+  console.log("🚀 ~ file: leetcode.js:732 ~ qTitle:", qTitle)
+  
   // Problem difficulty
-  difficulty = this.parseDifficulty();
+  difficulty = data.question.difficulty
+  // difficulty = this.parseDifficulty();
+  // console.log("🚀 ~ file: leetcode.js:717 ~ difficulty:", difficulty)
 
   // Final formatting of the contents of the README for each problem
   const markdown = `<h2><a href="${questionUrl}">${qTitle}</a></h2><h3>${difficulty}</h3><hr>${qBody}`;
@@ -777,15 +769,58 @@ LeetCodeV2.prototype.parseQuestionDescription = function () {
   }
   return description[0].content;
 };
-LeetCodeV2.prototype.parseQuestionNumber = function () {
-  const maxQuestionNumLength = 4;
-  let title = document.getElementsByClassName('mr-2 text-lg font-medium text-label-1');
-  if (!checkElem(title)) {
-    throw new Error('Could not parse question num');
-  }
+LeetCodeV2.prototype.parseQuestionNumber = async function () {
+  let questionId
+  if (!this.data) {
+    let options = {
+      method: 'GET',
+      headers: {
+        'Cookie': 'gr_user_id=c4ef5fad-30b7-46f1-b73e-b8c05fef2b62; csrftoken=aqFMRL1JOyXo7barS5s1zabOQly0baQCyOTHqzsLonZc3nc5A8lJJNTQW8vP5f8W',
+        'Host': 'leetcode.com',
+        'User-Agent': 'LeetHub/2.0'
+      }
+    }
+    const URL = window.location.href
+    console.log("🚀 ~ file: leetcode.js:755 ~ URL:", URL)
+    questionId = await fetch(URL, options)
+      .then(res => res.text()).then(html => {
+        console.log("🚀 ~ file: leetcode.js:758 ~ .then ~ html:", html)
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        return doc.getElementById('__NEXT_DATA__').innerText
+      }).then(data => {console.log(data); return JSON.parse(data)})
+      .then(data => {
+        this.data = data
+        console.log({data})
+        return data.props.pageProps.dehydratedState.queries[0].state.data.question.questionId
+      })
+      //POST leetcode.com/graphql/
+      // ZDBtQGyfNfvazDSZ4hFPTmdh5e1IP6d4PKCAaQy4b3iSSh1P9AMACwrjtUwrmsoF
+    //   let questionEditorDataQuery = {"query":"\n    query questionEditorData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    codeSnippets {\n      lang\n      langSlug\n      code\n    }\n    envInfo\n    enableRunCode\n  }\n}\n    ","variables":{"titleSlug":"two-sum"},"operationName":"questionEditorData"}
+    //   let options = {
+    //     method: 'POST',
+    //     headers: {
+    //       'cookie': 'gr_user_id=c4ef5fad-30b7-46f1-b73e-b8c05fef2b62; csrftoken=ZDBtQGyfNfvazDSZ4hFPTmdh5e1IP6d4PKCAaQy4b3iSSh1P9AMACwrjtUwrmsoF; 87b5a3c3f1a55520_gr_last_sent_cs1=arunbhardwaj; __stripe_mid=b6287d2b-af1f-487c-b0df-3d8c28618c94d5d51a; __atuvc=3%7C15; __atssc=google%3B1; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiMjQ1NzcxMCIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjA4ZWM0NWQ4MWJlMWEyMGNkNDRjYTkzYWJkMjhkZTY2NmZiYjRmNDQiLCJpZCI6MjQ1NzcxMCwiZW1haWwiOiJhcnVuLmIuOTVAZ21haWwuY29tIiwidXNlcm5hbWUiOiJhcnVuYmhhcmR3YWoiLCJ1c2VyX3NsdWciOiJhcnVuYmhhcmR3YWoiLCJhdmF0YXIiOiJodHRwczovL3MzLXVzLXdlc3QtMS5hbWF6b25hd3MuY29tL3MzLWxjLXVwbG9hZC9hc3NldHMvZGVmYXVsdF9hdmF0YXIuanBnIiwicmVmcmVzaGVkX2F0IjoxNjgxMzI1MDg2LCJpcCI6Ijk2LjIzMi4xMjMuMjAwIiwiaWRlbnRpdHkiOiI5ZWE2ZjU4MGJjMjE2YWUwM2YwMGQ5ZTVmZWU5NjY1ZiIsInNlc3Npb25faWQiOjM3Njc3MDk2LCJfc2Vzc2lvbl9leHBpcnkiOjEyMDk2MDB9.Tm9UHVoefdH_dhE7rre3pU75TQzHeOuVHo_P3OKCtoI; _gid=GA1.2.1438583426.1681484856; 87b5a3c3f1a55520_gr_session_id=4db4ed21-36c4-4c08-90c3-970a49f437e4; 87b5a3c3f1a55520_gr_last_sent_sid_with_cs1=4db4ed21-36c4-4c08-90c3-970a49f437e4; 87b5a3c3f1a55520_gr_session_id_4db4ed21-36c4-4c08-90c3-970a49f437e4=true; NEW_PROBLEMLIST_PAGE=1; _ga=GA1.1.2078669182.1680737411; __stripe_sid=fffebce6-c684-44d4-be4d-8d29b3fa7f60a8239c; 87b5a3c3f1a55520_gr_cs1=arunbhardwaj; _dd_s=rum=0&expire=1681496136410; _ga_CDRWKZTDEX=GS1.1.1681492989.14.1.1681495237.0.0.0',
+    //       'content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify(questionEditorDataQuery)
+    //   }
+    //   fetch('https://leetcode.com/graphql/', options)
 
-  title = title[0].innerText;
-  let num = title.split('.')[0];
+    // let query = {"query":"\n    query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {\n  problemsetQuestionList: questionList(\n    categorySlug: $categorySlug\n    limit: $limit\n    skip: $skip\n    filters: $filters\n  ) {\n    total: totalNum\n    questions: data {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      topicTags {\n        name\n        id\n        slug\n      }\n      hasSolution\n      hasVideoSolution\n    }\n  }\n}\n    ","variables":{"categorySlug":"","skip":0,"limit":50,"filters":{"searchKeywords":"Two Sum"}},"operationName":"problemsetQuestionList"}
+    
+  } else {
+    questionId = this.data.props.pageProps.dehydratedState.queries[0].state.data.question.questionId
+  }
+  
+  const maxQuestionNumLength = 4;
+  // let title = document.getElementsByClassName('mr-2 text-lg font-medium text-label-1');
+  // if (!checkElem(title)) {
+  //   throw new Error('Could not parse question num');
+  // }
+
+  // title = title[0].innerText;
+  // let num = title.split('.')[0];
+  let num = questionId
   if (num.length < maxQuestionNumLength) {
     num = '0'.repeat(4 - num.length) + num;
   }
@@ -802,9 +837,9 @@ LeetCodeV2.prototype.parseDifficulty = function () {
 };
 LeetCodeV2.prototype.getProblemNameSlug = function () {
   let questionTitle = convertToSlug(this.parseQuestionTitle());
-  let questionNum = this.parseQuestionNumber();
+  // let questionNum = this.parseQuestionNumber();
 
-  return questionNum + '-' + questionTitle;
+  return 'random' + '-' + questionTitle;
 };
 LeetCodeV2.prototype.getSuccessStateAndUpdate = function () {
   const successTag = document.querySelectorAll('[data-e2e-locator="submission-result"]');
@@ -882,43 +917,70 @@ LeetCodeV2.prototype.injectSpinnerStyle = function () {
   document.head.append(style);
 };
 
+/* Sync to local storage */
+chrome.storage.local.get('isSync', data => {
+  keys = [
+    'leethub_token',
+    'leethub_username',
+    'pipe_leethub',
+    'stats',
+    'leethub_hook',
+    'mode_type',
+  ];
+  if (!data || !data.isSync) {
+    keys.forEach(key => {
+      chrome.storage.sync.get(key, data => {
+        chrome.storage.local.set({ [key]: data[key] });
+      });
+    });
+    chrome.storage.local.set({ isSync: true }, data => {
+      console.log('LeetHub Synced to local values');
+    });
+  } else {
+    console.log('LeetHub Local storage already synced!');
+  }
+});
+
 let leetCode;
 const isLeetCodeV2 = document.getElementById('chakra-script') != null;
 if (!isLeetCodeV2) {
-  console.log('v1');
   leetCode = new LeetCodeV1();
 } else {
-  console.log('v2');
   leetCode = new LeetCodeV2();
 }
 
 const loader = () => {
   const intervalId = setInterval(async () => {
-    const isSuccessfulSubmission = leetCode.getSuccessStateAndUpdate();
-    console.log(
-      '🚀 ~ file: leetcode.js:878 ~ loader ~ isSuccessfulSubmission:',
-      isSuccessfulSubmission,
-    );
-    if (!isSuccessfulSubmission) {
-      return;
-    }
-
-    const probStats = leetCode.parseStats();
-    console.log('🚀 ~ file: leetcode.js:884 ~ loader ~ probStats:', probStats);
-    const probStatement = leetCode.parseQuestion();
-    console.log('🚀 ~ file: leetcode.js:886 ~ loader ~ probStatement:', probStatement);
-    if (!probStatement) {
-      console.error('Could not find problem statement');
-      return;
-    }
-
-    const problemName = leetCode.getProblemNameSlug();
-    console.log('🚀 ~ file: leetcode.js:893 ~ loader ~ problemName:', problemName);
-    const language = leetCode.findLanguage();
-    console.log('🚀 ~ file: leetcode.js:895 ~ loader ~ language:', language);
-    if (!language) {
-      console.error('Could not find language');
-      return;
+    try {
+      const isSuccessfulSubmission = leetCode.getSuccessStateAndUpdate();
+      console.log(
+        '🚀 ~ file: leetcode.js:878 ~ loader ~ isSuccessfulSubmission:',
+        isSuccessfulSubmission,
+      );
+      if (!isSuccessfulSubmission) {
+        return;
+      }
+  
+      // const probStats = leetCode.parseStats();
+      // console.log('🚀 ~ file: leetcode.js:884 ~ loader ~ probStats:', probStats);
+      const probStatement = await leetCode.parseQuestion();
+      console.log('🚀 ~ file: leetcode.js:886 ~ loader ~ probStatement:', probStatement);
+      if (!probStatement) {
+        console.error('Could not find problem statement');
+        return;
+      }
+  
+      // const problemName = leetCode.getProblemNameSlug();
+      // console.log('🚀 ~ file: leetcode.js:893 ~ loader ~ problemName:', problemName);
+      // const language = leetCode.findLanguage();
+      // console.log('🚀 ~ file: leetcode.js:895 ~ loader ~ language:', language);
+      // if (!language) {
+      //   console.error('Could not find language');
+      //   return;
+      // }
+    } catch (err) {
+      console.error(err)
+      clearInterval(intervalId)
     }
 
     // start upload indicator here
