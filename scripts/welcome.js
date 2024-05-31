d3b882f0971d08f272b4212800cb93c89ff95bcf
arunbhardwaj <arun.b.95@gmail.com>
2024-05-31 16:02:27 -0400

    updates description

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -83,7 +83,7 @@ const createRepo = (token, name) => {
     private: true,
     auto_init: true,
     description:
-      'Collection of LeetCode questions to ace the coding interview! - Created using [LeetHub v2](https://github.com/arunbhardwaj/LeetHub-2.0)',
+      'A collection of LeetCode questions to ace the coding interview! - Created using [LeetHub v2](https://github.com/arunbhardwaj/LeetHub-2.0)',
   };
   data = JSON.stringify(data);
 
